4c2c46b0f39f41cd3210ccb7c164e41ad04e77ad
Federico <fedevilla88@gmail.com>
2018-11-12 10:45:54 -0300

    first commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const loadProducts = () => {
     return dispatch => {
-        return axios.get(`https://www.repartienda.com/wp-json/wc/v1/products?category=22&per_page=30&consumer_key=ck_c5476ef4756a9191b91fa026dffa9bdc1aea42dc&consumer_secret=cs_2657e55b8b835273b00a45b901c1af84a72ff216`)
+        return axios.get(`http://localhost:3001/products`)
             .then(response => {
                 dispatch({
                     type: "REPLACE_PRODUCTS",
